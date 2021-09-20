a524832510e8420b3658d146a96661c3c74d9704
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-20 17:06:10 +0200

    footer bottom

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,26 @@
 <template>
-    <div></div>
+    <footer>
+        <div class="container">
+            <div class="footer-top">
+
+            </div>
+            <div class="footer-bottom">
+                <div class="sign-up-btn">
+                    <button>Sign-up Now!</button>
+                </div>
+                <div class="social-media-links">
+                    <ul>
+                        <li><a href="#">Follow Us</a></li>
+                        <li><a href="#"></a><img src="../assets/img/footer-facebook.png" alt=""></li>
+                        <li><a href="#"></a><img src="../assets/img/footer-twitter.png" alt=""></li>
+                        <li><a href="#"></a><img src="../assets/img/footer-youtube.png" alt=""></li>
+                        <li><a href="#"></a><img src="../assets/img/footer-pinterest.png" alt=""></li>
+                        <li><a href="#"></a><img src="../assets/img/footer-periscope.png" alt=""></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -9,5 +30,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @import "../style/General";
+    @import "../style/Vars";
 
+    .footer-bottom {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100px;
+        background-color: #303030;
+    }
+
+    .sign-up-btn {
+        padding: 20px;
+
+        button {
+            color: white;
+            text-transform: uppercase;
+            background-color: #303030;
+            border: 1px solid $primary;
+            padding: 10px 15px;
+        }
+    }
+
+    .social-media-links ul:first-child a {
+        color: $primary;
+        font-weight: bold;
+    }
+
+    .social-media-links ul {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        padding: 10px 15px;
+
+        li {
+            padding: 10px 10px;
+        }
+    }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -84,7 +84,6 @@ export default {
             display: flex;
             justify-content: space-between;
             align-items: center;
-            margin: 0 auto;
         }
 
         ul {
